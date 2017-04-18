4b78ef2f94036e77d8bd955c48629887d81487a7
Yves Cabral <yvescabral16@gmail.com>
2017-04-18 16:54:25 -0300

    layers finished

--- trace_reader_with_layers.rb
+++ trace_reader_with_layers.rb
@@ -1,29 +1,40 @@
 class CacheMemory
-  class Layer
-    attr_accessor :tag, :valid
-    def initialize()
-      @tag = nil
-      @valid = false
-    end
-  end
-  class Line
-    attr_accessor :layers
-    def initialize(layers=1)
-      @layers = []
-    end
-  end
-
   def initialize(positions, layers=1)
     @lines = []
+    @layers = layers
     for i in 0..positions-1
-      @lines << Line.new(layers)
+      @lines << []
     end
   end
 
   # Return true if is a hit and false if is a miss
-  def get(i, tag)
-    @lines[i].tag = tag
-    @lines[i].valid = true
+  def get(position, tag)
+    size = @lines[position].size
+    if size > 0
+      for i in 0..size-1
+        if @lines[position][i] == tag
+          if i < size-1
+            for j in i+1..size-1
+              @lines[position][j-1] = @lines[position][j-1]
+            end
+          end
+          @lines[position][size-1] = tag # Last recent used
+          return true # Hit
+        end
+      end
+      if size < @layers
+        @lines[position] << tag # Last recent used
+      else
+        for i in 1..size-1
+          @lines[position][i-1] = @lines[position][i]
+        end
+        @lines[position][size-1] = tag # Last recent used
+      end
+      return false # Miss
+    else
+      @lines[position] << tag # Last recent used
+      return false # Miss
+    end
   end
 end
 
@@ -31,9 +42,7 @@ def hex2bin(n)
   return n.hex.to_s(2).rjust(n.size*4, '0')
 end
 
-require 'json'
-
-acesso, miss, hit, hitrate, history = 0, 0, 0, 0, []
+acessos, miss, hit, hitrate = 0, 0, 0, 0
 puts "Quantas linhas terá a memória cache?"
 size = gets.to_i
 puts "Quantas colunas (palavras)?"
@@ -42,12 +51,12 @@ puts "Quantas camadas?"
 layers  = gets.to_i
 puts "Qual arquivo de trace deseja ler?"
 file = gets.chomp
-puts "Lendo #{file}.trace ..."
+puts "Lendo #{file} ..."
 memory = CacheMemory.new size, layers
 bits_size, bits_words = Math.log2(size).to_i, Math.log2(words).to_i
 
 File.open("traces/" + file, "r").each_line do |line|
-	if line[0] == 2 then
+	if line[0] == '2' then
 		line = line[2,8].rjust(8,'0')
 		tag = hex2bin(line)[0,32-bits_words]
 		index = tag.slice(-bits_size,bits_size).to_i(2)
@@ -56,11 +65,10 @@ File.open("traces/" + file, "r").each_line do |line|
 		else
 		  miss += 1
 		end
-#	end
+    acessos += 1
+	end
 end
 
-hitrate = (hit * 1.0 / (miss + hit))*100;
-puts "Hit: #{hit}\nMiss: #{miss}"
+hitrate = (hit * 1.0 / acessos)* 100;
+puts "Total de acessos: #{acesso}\nHit: #{hit}\nMiss: #{miss}"
 puts "Hit rate: %.2f%" % hitrate
-json = JSON.generate(history)
-File.open('data.js', 'w') { |file| file.write("var data = " + json + ";") }
